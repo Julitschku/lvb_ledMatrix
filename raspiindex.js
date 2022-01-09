b76680932fad062326b1e2aa14171c25bb80e7ee
Julitschku <Julianteichmann@gmx.de>
2022-01-09 20:37:06 +0100

    Merge remote-tracking branch 'origin/main'

---
+++ raspiindex.js
@@ -0,0 +1,89 @@
+const createClient = require('hafas-client')
+const vbbProfile = require('hafas-client/p/insa')
+const departures = require("lvb");
+const {date} = require("hafas-client/format");
+const {dateTime} = require("hafas-client/parse");
+const client = createClient(vbbProfile, 'my-awesome-program')
+const Jimp = require('Jimp')
+
+function Anzeigen(o){
+
+    var Bahn = new Array()
+    var Bahn = []
+    a = client.departures('11330', {
+        duration: 300,
+        direction: "13002",
+        products: {bus: false, suburban: false},
+        results: 2
+    }).then(baba => {
+        return baba
+    })
+        // .then(console.log)
+        .catch(console.error)
+
+    a.then(function (result) {
+        for (i = 0; i < 2; i++) {
+            var ankunft = Date.parse(result[i].plannedWhen)
+            var delay = result[i].delay
+            var zeitinmin = ((ankunft - new Date() + delay) / 60000)
+            var eintrag = {
+                Linie: 4,
+                Ankunft: Math.round(zeitinmin),
+                Verspätung: delay/60
+            }
+            Bahn.push(eintrag)
+        }
+
+        b = client.departures('12083', {
+            duration: 300,
+            direction: "13002",
+            products: {bus: false, suburban: false},
+            results: 2
+        }).then(baba => {
+            return baba
+        })
+            // .then(console.log)
+            .catch(console.error)
+
+        b.then(function (result) {
+            for (i = 0; i < 2; i++) {
+                var ankunft = Date.parse(result[i].plannedWhen)
+                var delay = result[i].delay
+                var zeitinmin = ((ankunft - new Date() + delay) / 60000)
+                var eintrag = {
+                    Linie: 15,
+                    Ankunft: Math.round(zeitinmin).toString(),
+                    Verspätung: delay/60
+                }
+                Bahn.push(eintrag)
+            }
+            /* Bahn.sort(function (a, b) {
+                 return a.Ankunft - b.Ankunft;
+             })*/
+
+            Jimp.read("img.png")
+                .then(function (image) {
+                    loadedImage = image;
+                    return Jimp.loadFont("/home/pi/lvb_ledMatrix/PressStart2P-vaV7.ttf-5/16aPkv_cojdMp4jRi15f5RX3.ttf.fnt");
+                })
+                .then(function (font) {
+                    loadedImage.print(font, 0, 8,Bahn[0].Ankunft )
+                    loadedImage.print(font, 0, 16, Bahn[1].Ankunft)
+                    loadedImage.print(font, 32, 8, Bahn[2].Ankunft)
+                    loadedImage.print(font, 32, 16, Bahn[3].Ankunft)
+                        .write("img2.png")
+                        (Anzeigen(++o));
+
+                })
+                .catch(function (err) {
+                    console.error(err);
+                });
+            console.log(Bahn)
+            console.log(o)
+
+        })
+    })
+}
+
+Anzeigen(0);
+
