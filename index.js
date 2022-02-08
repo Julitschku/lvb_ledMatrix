24ba2ac4b3cd6d7349e7cf9684bf4f627f4afc7a
Julitschku <Julianteichmann@gmx.de>
2022-02-08 15:11:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ function Anzeigen(o){
             var ankunft = Date.parse(result[i].plannedWhen)
             var delay = result[i].delay
             var zeitinmin = ((ankunft - new Date() + delay) / 60000)
+            if(delay>0) delay="+" + String(delay/60)
+            else if(delay<0) delay="+" + String(delay/60)
+            else delay=""
+            var eintrag = {
+                Linie: 15,
+                Ankunft: Math.round(zeitinmin).toString(),
+                Verspätung:delay
+            }
             var eintrag = {
                 Linie: 4,
                 Ankunft: Math.round(zeitinmin),
@@ -53,10 +61,13 @@ function Anzeigen(o){
                 }
                 var delay = result[i].delay
                 var zeitinmin = ((ankunft - new Date() + delay) / 60000)
+                if(delay>0) delay="+" + String(delay/60)
+                else if(delay<0) delay="+" + String(delay/60)
+                else delay=""
                 var eintrag = {
                     Linie: 15,
                     Ankunft: Math.round(zeitinmin).toString(),
-                    Verspätung: delay/60
+                    Verspätung:delay
                 }
                 Bahn.push(eintrag)
             }
@@ -70,10 +81,10 @@ function Anzeigen(o){
                     return Jimp.loadFont("/Users/julian/IdeaProjects/lvb_raspi/PressStart2P-vaV7.ttf-5/16aPkv_cojdMp4jRi15f5RX3.ttf.fnt");
                 })
                 .then(function (font) {
-                    loadedImage.print(font, 0, 8,Bahn[0].Ankunft )
-                    loadedImage.print(font, 0, 16, Bahn[1].Ankunft)
-                    loadedImage.print(font, 32, 8, Bahn[2].Ankunft)
-                    loadedImage.print(font, 32, 16, Bahn[3].Ankunft)
+                    loadedImage.print(font, 0, 8,Bahn[0].Ankunft + Bahn[0].Verspätung )
+                    loadedImage.print(font, 0, 16, Bahn[1].Ankunft + Bahn[1].Verspätung)
+                    loadedImage.print(font, 32, 8, Bahn[2].Ankunft+ Bahn[2].Verspätung)
+                    loadedImage.print(font, 32, 16, Bahn[3].Ankunft+ Bahn[3].Verspätung)
                         .write("img2.jpg")
                     Anzeigen(++o);
 
